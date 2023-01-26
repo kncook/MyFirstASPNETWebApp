ac49ae214cf49c7ea2c41dcfce39905b10f54a52
kncook <katherine.n.cook3@gmail.com>
2023-01-25 20:35:39 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/GradeCalc.js
@@ -0,0 +1,15 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("From: " + document.getElementById("g1").value +
+////        ", Subject: " + document.getElementById("g2").value +
+////        ", Message: " + document.getElementById("g3").value);
+////})
+
+
+/*jquery way of doing it*/
+$("#btnSend").click(function() {
+    alert("From: " + $("#g1").val() +
+        ", Subject: " + $("#g2").val() +
+        ", Message: " + $("#g3").val());
+    $("#picKat").fadeout("slow");
+/*fadetoggle() when clicked once it fades out and clicked twice it fades in*/
+})
